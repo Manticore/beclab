481cbc212b90f4fc9243dc3fae63b662ad53d7e3
Bogdan Opanchuk <bogdan@opanchuk.net>
2013-12-17 18:48:24 +1100

    test/soliton: added the problem description

--- test/soliton.py
+++ test/soliton.py
@@ -1,3 +1,83 @@
+r"""
+This example applies different steppers to the problem of simulating
+a bright soliton in a 1D BEC with attractive interaction.
+
+For the GPE equation
+
+.. math::
+
+    i \hbar \frac{\partial \Psi(x, t)}{\partial t}
+    = \left(
+        - \frac{\hbar^2}{2m} \nabla^2
+        + U |\Psi|^2
+    \right) \Psi
+
+there is an exact solution
+
+.. math::
+
+    \Psi(x, t)
+    = \sqrt{n_0} \exp(-i \mu t / \hbar)
+        \frac{1}{\cosh\left(
+            \sqrt{2 m |\mu| / \hbar^2} x
+        \right)},
+
+where :math:`\mu = g n_0 / 2`.
+The attractive interaction means that :math:`U < 0`.
+In dimensionless variables
+:math:`x = \hbar \tilde{x} / \sqrt{m g n_0}`,
+:math:`t = \hbar \tilde{t} / \mu`,
+the GPE has the form
+
+.. math::
+
+    i \frac{\partial \tilde{\Psi}(\tilde{x}, \tilde{t})}{\partial \tilde{t}}
+    = \left(
+        - \tilde{\nabla}^2
+        - 2 |\tilde{\Psi}|^2 / \tilde{n}_0
+    \right) \tilde{\Psi},
+
+and the soliton solution is
+
+.. math::
+
+    \tilde{\Psi}(\tilde{x}, \tilde{t})
+    = \sqrt{\tilde{n_0}} \exp(-i \tilde{t}) \frac{1}{\cosh \tilde{x}},
+
+We can introduce linear losses in this GPE as
+
+.. math::
+
+    i \frac{\partial \tilde{\Psi}(\tilde{x}, \tilde{t})}{\partial \tilde{t}}
+    = \left(
+        - \tilde{\nabla}^2
+        - 2 |\tilde{\Psi}|^2 / \tilde{n}_0
+    \right) \tilde{\Psi}
+    - i \gamma \tilde{\Psi},
+
+and in Wigner representation the equation will be
+
+.. math::
+
+    \frac{\partial \tilde{\Psi}(\tilde{x}, \tilde{t})}{\partial \tilde{t}}
+    = -i \left(
+        - \tilde{\nabla}^2
+        - 2 (|\tilde{\Psi}|^2 - M / V) / \tilde{n}_0
+    \right) \tilde{\Psi}
+    - \gamma \tilde{\Psi}
+    + \gamma dW(\tilde{x}, \tilde{t}),
+
+where :math:`M` is the number of modes, :math:`V` is the box size,
+and :math:`dW` is the standard Wiener process.
+
+In this example we test that:
+
+- without losses the numerical solution converges to the exact solution;
+- the total atom number is preserved without losses;
+- the total atom number decreases as :math:`N_0 \exp(-2 \gamma \tilde{t})`
+  when the losses are enabled, both for the GPE and for the Wigner representation.
+"""
+
 from __future__ import print_function, division
 
 import numpy
@@ -111,8 +191,8 @@ def run_test(thr, label, stepper_cls, no_losses=False, wigner=False):
     samples = 100 # how many samples to take during simulation
     steps = samples * 100 # number of time steps (should be multiple of samples)
     gamma = 0.0 if no_losses else 0.2
-    soliton_height = 10.0
-    U = -2. / soliton_height ** 2
+    n0 = 100.0
+    U = -2. / n0
     complex_dtype = numpy.complex128
     float_dtype = dtypes.real_for(complex_dtype)
     seed = 1234
@@ -122,7 +202,7 @@ def run_test(thr, label, stepper_cls, no_losses=False, wigner=False):
     dx = x[1] - x[0]
 
     # Initial state
-    psi0 = soliton_height / numpy.cosh(x)
+    psi0 = numpy.sqrt(n0) / numpy.cosh(x)
     N0 = (numpy.abs(psi0) ** 2).sum() * dx
     n_max = (numpy.abs(psi0) ** 2).max()
     print("N0 =", N0)
@@ -192,7 +272,7 @@ def run_test(thr, label, stepper_cls, no_losses=False, wigner=False):
         psi = result['psi']
         tt = numpy.linspace(0, interval, samples + 1, endpoint=True).reshape(samples + 1, 1, 1, 1)
         xx = x.reshape(1, 1, 1, x.size)
-        psi_exact = soliton_height / numpy.cosh(xx) * numpy.exp(1j * tt)
+        psi_exact = numpy.sqrt(n0) / numpy.cosh(xx) * numpy.exp(1j * tt)
         diff = numpy.linalg.norm(psi - psi_exact) / numpy.linalg.norm(psi_exact)
         print("Difference with an exact solution:", diff)
         assert diff < 1e-3
