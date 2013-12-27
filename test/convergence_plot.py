07a57de53a853eb7780dd19543985cf8740cbe27
Bogdan Opanchuk <bogdan@opanchuk.net>
2013-12-27 15:58:46 +1100

    test/convergence_plot: added normalised plot

--- test/convergence_plot.py
+++ test/convergence_plot.py
@@ -46,6 +46,39 @@ fig.savefig('convergence_vals.pdf')
 
 
 fig = plt.figure()
+s = fig.add_subplot(111, xlabel='Time (s)', ylabel='Convergence')
+s.set_xscale('log', basex=10)
+s.set_yscale('log', basey=10)
+for label in labels:
+    times = []
+    step_nums = []
+    strong_errors = []
+    N_errors = []
+    N_diffs = []
+    SZ2_errors = []
+    for steps in sorted(results[label]):
+        result = results[label][steps]
+        times.append(result['time'])
+        step_nums.append(steps)
+        strong_errors.append(result['strong_error'])
+        N_errors.append(result['N_convergence'])
+        SZ2_errors.append(result['SZ2_convergence'])
+        N_diffs.append(result['N_diff'])
+
+    s.plot(times, strong_errors, label=label + ", strong errors",
+        color=colors[label], linestyle='-')
+    s.plot(times, N_errors, label=label + ", N errors",
+        color=colors[label], linestyle='--')
+    s.plot(times, SZ2_errors, label=label + ", Sz^2 errors",
+        color=colors[label], linestyle=':')
+    #s.plot(step_nums, N_diffs, label=label + ", N diffs",
+    #    color=colors[label], linestyle='-.')
+
+s.legend(fontsize=5, loc='lower left')
+fig.savefig('convergence_vals_normalized.pdf')
+
+
+fig = plt.figure()
 s = fig.add_subplot(111, xlabel='Steps', ylabel='Time (s)')
 s.set_xscale('log', basex=10)
 s.set_yscale('log', basey=10)
