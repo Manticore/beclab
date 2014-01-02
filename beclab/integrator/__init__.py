9d84319f2530b4aa48092efbeda7ceb32ef4e247
Bogdan Opanchuk <bogdan@opanchuk.net>
2014-01-02 16:10:40 +1100

    integrator: fixed import

--- beclab/integrator/__init__.py
+++ beclab/integrator/__init__.py
@@ -2,7 +2,7 @@ from beclab.integrator.integrator import Integrator, StopIntegration, Sampler
 from beclab.integrator.wiener import Wiener
 from beclab.integrator.modules import Drift, Diffusion
 
-from beclab.integrator.sscd_stepper import CDIPStepper
+from beclab.integrator.cdip_stepper import CDIPStepper
 from beclab.integrator.cd_stepper import CDStepper
 from beclab.integrator.rk4ip_stepper import RK4IPStepper
 from beclab.integrator.rk46nl_stepper import RK46NLStepper
