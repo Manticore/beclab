56cea5368459adbab462cc3269c87bcb4d67a3be
Bogdan Opanchuk <bogdan@opanchuk.net>
2014-01-14 00:42:42 +1100

    samplers: returning the imaginary value of I

--- beclab/samplers.py
+++ beclab/samplers.py
@@ -48,12 +48,7 @@ class InteractionSampler(Sampler):
 
     def __call__(self, psi, t):
         psi = psi.get()
-
-        density = numpy.abs(psi) ** 2 - (0.5 / self._grid.dV if self._wigner else 0)
-
-        I = numpy.abs((psi[:,0].conj() * psi[:,1]).sum((1, 2, 3))) * self._grid.dV
-
-        return I
+        return (psi[:,0].conj() * psi[:,1]).sum((1, 2, 3)) * self._grid.dV
 
 
 class VisibilitySampler(Sampler):
