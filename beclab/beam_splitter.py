0619e8a46a7ac4b45c54a37a5e42172db28e8e75
Bogdan Opanchuk <bogdan@opanchuk.net>
2014-01-27 22:21:29 +1100

    beam_splitter: theta noise support

--- beclab/beam_splitter.py
+++ beclab/beam_splitter.py
@@ -7,12 +7,13 @@ from reikna.algorithms import PureParallel
 
 def get_splitter(state_arr, comp1, comp2):
     real_dtype = dtypes.real_for(state_arr.dtype)
+    trajectories = state_arr.shape[0]
     return PureParallel(
         [
             Parameter('output', Annotation(state_arr, 'o')),
             Parameter('input', Annotation(state_arr, 'i')),
-            Parameter('theta', Annotation(Type(real_dtype))),
-            Parameter('phi', Annotation(Type(real_dtype)))],
+            Parameter('thetas', Annotation(Type(real_dtype, trajectories))),
+            Parameter('phis', Annotation(Type(real_dtype, trajectories)))],
         """
         <%
             trajectory = idxs[0]
@@ -23,15 +24,18 @@ def get_splitter(state_arr, comp1, comp2):
         const ${output.ctype} psi_${comp} = ${input.load_idx}(${trajectory}, ${comp}, ${coords});
         %endfor
 
-        ${r_ctype} sin_half_theta = sin(${theta} / 2);
-        ${r_ctype} cos_half_theta = cos(${theta} / 2);
+        const ${thetas.ctype} theta = ${thetas.load_idx}(${trajectory});
+        const ${phis.ctype} phi = ${phis.load_idx}(${trajectory});
+
+        ${r_ctype} sin_half_theta = sin(theta / 2);
+        ${r_ctype} cos_half_theta = cos(theta / 2);
 
         ${output.ctype} minus_i = COMPLEX_CTR(${output.ctype})(0, -1);
         ${output.ctype} k2 = ${mul_sr}(
-            ${mul_ss}(minus_i, ${polar_unit}(-${phi})),
+            ${mul_ss}(minus_i, ${polar_unit}(-phi)),
             sin_half_theta);
         ${output.ctype} k3 = ${mul_sr}(
-            ${mul_ss}(minus_i, ${polar_unit}(${phi})),
+            ${mul_ss}(minus_i, ${polar_unit}(phi)),
             sin_half_theta);
 
         const ${output.ctype} psi_${comp1}_new =
@@ -62,34 +66,54 @@ class BeamSplitterMatrix(Computation):
     def __init__(self, state_arr, comp1, comp2):
 
         real_dtype = dtypes.real_for(state_arr.dtype)
+        trajectories = state_arr.shape[0]
         Computation.__init__(self,
             [Parameter('output', Annotation(state_arr, 'o')),
             Parameter('input', Annotation(state_arr, 'i')),
-            Parameter('theta', Annotation(Type(real_dtype))),
-            Parameter('phi', Annotation(Type(real_dtype)))])
+            Parameter('thetas', Annotation(Type(real_dtype, trajectories))),
+            Parameter('phis', Annotation(Type(real_dtype, trajectories)))])
 
         self._splitter = get_splitter(state_arr, comp1, comp2)
 
-    def _build_plan(self, plan_factory, device_params, output, input_, theta, phi):
+    def _build_plan(self, plan_factory, device_params, output, input_, thetas, phis):
 
         plan = plan_factory()
-        plan.computation_call(self._splitter, output, input_, theta, phi)
+        plan.computation_call(self._splitter, output, input_, thetas, phis)
         return plan
 
 
 class BeamSplitter:
 
     def __init__(self, wfs_meta, comp1_num=0, comp2_num=1,
-            starting_phase=0, f_detuning=0, f_rabi=0):
+            starting_phase=0, f_detuning=0, f_rabi=0, seed=None):
 
+        self._rng = numpy.random.RandomState(seed=seed)
         self._starting_phase = starting_phase
         self._detuning = 2 * numpy.pi * f_detuning
         self._f_rabi = f_rabi
+        self._trajectories = wfs_meta.trajectories
+        self._thread = wfs_meta.thread
+        self._real_dtype = dtypes.real_for(wfs_meta.dtype)
         self._splitter = BeamSplitterMatrix(
             wfs_meta.data, comp1_num, comp2_num).compile(wfs_meta.thread)
 
-    def __call__(self, wfs_data, t, theta):
+    def __call__(self, wfs_data, t, theta, theta_noise=0, phi_noise=0):
         phi = t * self._detuning + self._starting_phase
         t_pulse = (theta / numpy.pi / 2.0) / self._f_rabi
-        self._splitter(wfs_data, wfs_data, theta, phi)
+
+        # TODO: use GPU RNG?
+        if phi_noise > 0.0:
+            phis = self._rng.normal(size=self._trajectories, scale=phi_noise, loc=phi)
+        else:
+            phis = numpy.ones(self._trajectories) * phi
+
+        if theta_noise > 0.0:
+            thetas = self._rng.normal(size=self._trajectories, scale=theta_noise, loc=theta)
+        else:
+            thetas = numpy.ones(self._trajectories) * theta
+
+        thetas = self._thread.to_device(thetas.astype(self._real_dtype))
+        phis = self._thread.to_device(phis.astype(self._real_dtype))
+
+        self._splitter(wfs_data, wfs_data, thetas, phis)
         return t + t_pulse
